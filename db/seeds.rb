426430f1776dd09f47cbc6e2cde43d07d4668cba
Elson Otake <elsonotake@yahoo.com.br>
2023-01-18 19:25:41 -0300

    Add seeds to article table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,158 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+articles = [
+  {
+    text: 'ALGOL is a family of imperative computer programming languages originally developed in 1958. ALGOL h'\
+          'eavily influenced many other languages and was the standard method for algorithm description used by'\
+          ' the Association for Computing Machinery in textbooks and academic sources for more than thirty year'\
+          's.'
+  },
+  {
+    text: 'Angular is a TypeScript-based, free and open-source web application framework led by the Angular Tea'\
+          'm at Google and by a community of individuals and corporations. Angular is a complete rewrite from t'\
+          'he same team that built AngularJS.'
+  },
+  {
+    text: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web'\
+          ' development. It contains HTML, CSS and JavaScript-based design templates for typography, forms, but'\
+          'tons, navigation, and other interface components.'
+  },
+  {
+    text: 'C# is a general-purpose programming language supporting multiple paradigms. C# encompasses static ty'\
+          'ping, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented'\
+          ', and component-oriented programming disciplines.'
+  },
+  {
+    text: 'C++ is a high-level general-purpose programming language created by Danish computer scientist Bjarne'\
+          ' Stroustrup as an extension of the C programming language, or "C with Classes".'
+  },
+  {
+    text: 'COBOL is a compiled English-like computer programming language designed for business use. It is an i'\
+          'mperative, procedural and, since 2002, object-oriented language. COBOL is primarily used in business'\
+          ', finance, and administrative systems for companies and governments.'
+  },
+  {
+    text: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document '\
+          'written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide '\
+          'Web, alongside HTML and JavaScript.'
+  },
+  {
+    text: 'Django is a free and open-source, Python-based web framework that follows the model–template–views a'\
+          'rchitectural pattern. It is maintained by the Django Software Foundation, an independent organizatio'\
+          'n established in the US as a 501 non-profit.'
+  },
+  {
+    text: 'Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob '\
+          'Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection'\
+          ', structural typing, and CSP-style concurrency.'
+  },
+  {
+    text: 'GraphQL is an open-source data query and manipulation language for APIs, and a runtime for fulfillin'\
+          'g queries with existing data. GraphQL was developed internally by Facebook in 2012 before being publ'\
+          'icly released in 2015.'
+  },
+  {
+    text: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be d'\
+          'isplayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scr'\
+          'ipting languages such as JavaScript.'
+  },
+  {
+    text: 'Java is a set of computer software and specifications developed by James Gosling at Sun Microsystems'\
+          ', which was later acquired by the Oracle Corporation, that provides a system for developing applicat'\
+          'ion software and deploying it in a cross-platform computing environment.'
+  },
+  {
+    text: 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies '\
+          'of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the cli'\
+          'ent side for webpage behavior, often incorporating third-party libraries.'
+  },
+  {
+    text: 'Laravel is a free and open-source PHP web framework, created by Taylor Otwell and intended for the d'\
+          'evelopment of web applications following the model view controller architectural pattern and based o'\
+          'n Symfony.'
+  },
+  {
+    text: 'Lisp is a family of programming languages with a long history and a distinctive, fully parenthesized'\
+          ' prefix notation. Originally specified in 1960, Lisp is the second-oldest high-level programming lan'\
+          'guage still in common use, after Fortran.'
+  },
+  {
+    text: 'MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoS'\
+          'QL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by'\
+          ' MongoDB Inc. and licensed under the Server Side Public License which is deemed non-free by several '\
+          'distributions.'
+  },
+  {
+    text: 'Pascal is an imperative and procedural programming language, designed by Niklaus Wirth as a small, e'\
+          'fficient language intended to encourage good programming practices using structured programming and '\
+          'data structuring. It is named in honour of the French mathematician, philosopher and physicist Blais'\
+          'e Pascal.'
+  },
+  {
+    text: 'PHP is a general-purpose scripting language geared toward web development. It was originally created'\
+          ' by Danish-Canadian programmer Rasmus Lerdorf in 1993 and released in 1995. The PHP reference implem'\
+          'entation is now produced by The PHP Group.'
+  },
+  {
+    text: 'PostgreSQL, also known as Postgres, is a free and open-source relational database management system '\
+          'emphasizing extensibility and SQL compliance. It was originally named POSTGRES, referring to its ori'\
+          'gins as a successor to the Ingres database developed at the University of California, Berkeley.'
+  },
+  {
+    text: 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code '\
+          'readability with the use of significant indentation. Python is dynamically-typed and garbage-collect'\
+          'ed. It supports multiple programming paradigms, including structured, object-oriented and functional'\
+          ' programming.'
+  },
+  {
+    text: 'React is a free and open-source front-end JavaScript library for building user interfaces based on U'\
+          'I components. It is maintained by Meta and a community of individual developers and companies.'
+  },
+  {
+    text: 'A dynamic, open source programming language with a focus on simplicity and productivity. It has an e'\
+          'legant syntax that is natural to read and easy to write.'
+  },
+  {
+    text: 'Ruby on Rails is a server-side web application framework written in Ruby under the MIT License. Rail'\
+          's is a model view controller framework, providing default structures for a database, a web service, '\
+          'and web pages.'
+  },
+  {
+    text: 'Sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheet'\
+          's. SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, '\
+          'called "the indented syntax," uses a syntax similar to Haml.'
+  },
+  {
+    text: 'Structured Query Language, abbreviated as SQL, is a domain-specific language used in programming and'\
+          ' designed for managing data held in a relational database management system, or for stream processin'\
+          'g in a relational data stream management system.'
+  },
+  {
+    text: 'Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. an'\
+          'd the open-source community.'
+  },
+  {
+    text: 'Tailwind CSS is an open source CSS framework. The main feature of this library is that, unlike other'\
+          ' CSS frameworks like Bootstrap, it does not provide a series of predefined classes for elements such'\
+          ' as buttons or tables.'
+  },
+  {
+    text: 'TypeScript is a free and open source programming language developed and maintained by Microsoft. It '\
+          'is a strict syntactical superset of JavaScript and adds optional static typing to the language. It i'\
+          's designed for the development of large applications and transpiles to JavaScript.'
+  },
+  {
+    text: 'The original Visual Basic is a third-generation event-driven programming language from Microsoft kno'\
+          'wn for its Component Object Model programming model first released in 1991 and declared legacy durin'\
+          'g 2008. Microsoft intended Visual Basic to be relatively easy to learn and use.'
+  },
+  {
+    text: 'Vue.js is an open-source model view viewmodel front end JavaScript framework for building user inter'\
+          'faces and single-page applications. It was created by Evan You, and is maintained by him and the res'\
+          't of the active core team members.'
+  }
+]
+
+articles.each do |article|
+  article = Article.new
+  article.text = info[:text]
+  article.save
+end
