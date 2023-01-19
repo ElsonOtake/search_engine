70af52f37e5014615e0cca47a28cd042f6ce2af0
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 14:38:16 -0300

    Update controller actions

--- app/controllers/api/v1/analytics_controller.rb
+++ app/controllers/api/v1/analytics_controller.rb
@@ -1,53 +1,31 @@
 class Api::V1::AnalyticsController < ApplicationController
-  before_action :set_analytic, only: %i[ show update destroy ]
+  protect_from_forgery with: :null_session
 
   # GET /api/v1/analytics
-  # GET /api/v1/analytics.json
   def index
     @analytics = Analytic.all
+    render json: @analytics
   end
 
   # GET /api/v1/analytics/1
-  # GET /api/v1/analytics/1.json
   def show
   end
 
   # POST /api/v1/analytics
-  # POST /api/v1/analytics.json
   def create
     @analytic = Analytic.new(analytic_params)
-
+    puts "analytic_params #{analytic_params}"
     if @analytic.save
-      render :show, status: :created, location: @analytic
+      render json: @analytic, status: :created
     else
       render json: @analytic.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /api/v1/analytics/1
-  # PATCH/PUT /api/v1/analytics/1.json
-  def update
-    if @analytic.update(analytic_params)
-      render :show, status: :ok, location: @analytic
-    else
-      render json: @analytic.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /api/v1/analytics/1
-  # DELETE /api/v1/analytics/1.json
-  def destroy
-    @analytic.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_analytic
-      @analytic = Analytic.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
+    # Only allow a list of trusted Body parameters through.
     def analytic_params
-      params.fetch(:analytic, {})
-    end
+      allowed_data = %(person keyword results).freeze
+      json_payload.select { |allow| allowed_data.include?(allow) }    end
 end
