6e15241a425cb61d324bab28b4b0c4c0e3967504
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 19:06:19 -0300

    Add group by and order by to the analytics query

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,7 +3,11 @@ class AnalyticsController < ApplicationController
 
   def index
     @person = Person.find_by_id(params[:person_id])
-    @analytics = Analytic.where(person_id: @person)
+    @analytics = Analytic.
+                   where(person_id: @person).
+                   select("keyword, count(keyword) as search_count, avg(results) as results, max(created_at) as created_at").
+                   group("keyword").
+                   order('search_count desc, created_at desc')
   end
 
   def show
