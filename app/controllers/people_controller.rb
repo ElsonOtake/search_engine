77af06e02e3bcfac387d37db5b21e766ef6c8bd6
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 11:10:20 -0300

    Update controller actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,22 @@
 class PeopleController < ApplicationController
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.create(person_params)
+    if @person.valid?
+      session[:person_id] = @person.id
+      redirect_to person_coaches_path(@person.id)
+    else
+      flash[:error] = 'Error: please try to create an account again.'
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name)
+  end
 end
