6e05010b019fccbc56e87af294a54e366eae8728
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 14:07:06 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_135443) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_170055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "analytics", force: :cascade do |t|
+    t.string "keyword"
+    t.integer "results"
+    t.bigint "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_analytics_on_person_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "text", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_19_135443) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "analytics", "people"
 end
