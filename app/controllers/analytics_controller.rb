11be50ae6d347570177df7d740b3834a49c2f4f2
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 16:11:13 -0300

    Add index action

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,2 +1,21 @@
 class AnalyticsController < ApplicationController
+  before_action :set_analytic, only: [:show]
+
+  def index
+    @person = Person.find_by_id(params[:person_id])
+    @analytics = Analytic.where(person_id: @person)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_analytic
+    @analytic = Analytic.find(params[:id])
+  end
+
+  def analytic_params
+    params.require(:person_analytic).permit(:person_id, :id)
+  end
 end
