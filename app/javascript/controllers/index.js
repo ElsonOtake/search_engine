816de1f081d0c9b5ccc62683b5f9dd48ce214838
Elson Otake <elsonotake@yahoo.com.br>
2023-01-18 21:13:52 -0300

    Remove remained reference to Hello controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import SearchController from "./search_controller"
 application.register("search", SearchController)
