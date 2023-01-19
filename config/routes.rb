9ae57226296a902f868cd88393ab25090db5e143
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 11:10:49 -0300

    Add people resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles, only: %i[index show]
+  resources :people, only: %i[new create] do
+    resources :articles, only: %i[index show]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
