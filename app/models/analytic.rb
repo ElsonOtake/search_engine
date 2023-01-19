69eb5c0f4aa28dd1bf171d340990c3e9d12251a6
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 15:53:08 -0300

    Add validation to analytics

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,3 +1,5 @@
 class Analytic < ApplicationRecord
   belongs_to :person
+  validates :keyword, presence: true
+  validates :results, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
