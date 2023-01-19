339dcaf30e278e9e7c147f4c078f7c0575c35801
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 20:05:59 -0300

    Update params

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,25 +1,16 @@
 class AnalyticsController < ApplicationController
-  before_action :set_analytic, only: [:show]
 
   def index
-    @person = Person.find_by_id(params[:person_id])
-    @analytics = Analytic.
-                   where(person_id: @person).
+    @person = Person.find_by_id(analytic_params[:person_id])
+    @analytics = @person.analytics.
                    select("keyword, count(keyword) as search_count, avg(results) as results, max(created_at) as created_at").
                    group("keyword").
                    order('search_count desc, created_at desc')
   end
 
-  def show
-  end
-
   private
 
-  def set_analytic
-    @analytic = Analytic.find(params[:id])
-  end
-
   def analytic_params
-    params.require(:person_analytic).permit(:person_id, :id)
+    params.permit(:person_id)
   end
 end
