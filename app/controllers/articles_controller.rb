69a5e9d9120fcab680b4d543a0345c3dc8337a81
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 20:07:52 -0300

    Remove unused code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
 
   def index
     @articles = Article.all
   end
-
-  def show
-  end
-
-  private
-
-  def set_article
-    @article = Article.find(params[:id])
-  end
-
-  def article_params
-    params.require(:article).permit(:text)
-  end
+  
 end
