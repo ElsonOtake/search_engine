07ea025a6f1afbabd6c040fec168aedece63bf8d
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 15:51:11 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_170055) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_184846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "analytics", force: :cascade do |t|
     t.string "keyword"
-    t.integer "results"
+    t.integer "results", default: 0
     t.bigint "person_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
