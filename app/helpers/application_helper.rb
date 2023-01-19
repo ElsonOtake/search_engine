e14e548318165651de5aa791e0a43589f2f12939
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 13:33:47 -0300

    Add user methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logged_in?
+    !!session[:person_id]
+  end
+
+  def current_person
+    @current_person ||= Person.find_by_id(session[:person_id]) if !!session[:person_id]
+  end
 end
