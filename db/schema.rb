b5a9fb14911dc94f2e19f32c4dfccc9bc4ba93e6
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 10:56:07 -0300

    Create people table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_215620) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_135443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
