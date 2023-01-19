2f9caadf169cc4414316a3117ca3c8ccc0981f4e
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 10:40:04 -0300

    Setup timer to store keyword

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,8 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = [ "keyword" ]
-  
+  static targets = [ "keyword" ];
+
   clear() {
     const input = document.querySelector("input");
     const articles = document.querySelectorAll("article");
@@ -14,6 +14,10 @@ export default class extends Controller {
   }
 
   input() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      console.log('keyword', this.keyword);
+    }, 3000)
     const articles = document.querySelectorAll("article")
     articles.forEach(article => {
       const isVisible = this.keyword.every((key) => article.firstChild.nextSibling.text.toLowerCase().includes(key))
