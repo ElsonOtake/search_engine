a63820955f44607116b1614459b8267fca1a7e55
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 15:06:58 -0300

    Update create action

--- app/controllers/api/v1/analytics_controller.rb
+++ app/controllers/api/v1/analytics_controller.rb
@@ -13,8 +13,11 @@ class Api::V1::AnalyticsController < ApplicationController
 
   # POST /api/v1/analytics
   def create
+    @person = Person.find(params[:person_id])
     @analytic = Analytic.new(analytic_params)
+    @analytic.person_id = @person.id
     puts "analytic_params #{analytic_params}"
+    
     if @analytic.save
       render json: @analytic, status: :created
     else
@@ -24,8 +27,13 @@ class Api::V1::AnalyticsController < ApplicationController
 
   private
 
-    # Only allow a list of trusted Body parameters through.
-    def analytic_params
-      allowed_data = %(person keyword results).freeze
-      json_payload.select { |allow| allowed_data.include?(allow) }    end
+  def analytic_params
+    params.require(:person_analytics).permit(:person_id)
+  end
+  
+  # Only allow a list of trusted Body parameters through.
+  def analytic_params
+    allowed_data = %(keyword results).freeze
+    json_payload.select { |allow| allowed_data.include?(allow) }
+  end
 end
