693be71473b56cd7cedee5716b899468b75ea925
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 14:38:38 -0300

    Add json reader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def json_payload
+    return [] if request.raw_post.empty?
+  
+    HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
+  end
 end
