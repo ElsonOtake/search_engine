d77c289f6511f993b38c0833b1bd91da115cf3ad
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 15:06:41 -0300

    Update people controller methods

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,41 +1,39 @@
 class Api::V1::PeopleController < ApplicationController
+  protect_from_forgery with: :null_session
   before_action :set_person, only: %i[ show update destroy ]
 
   # GET /api/v1/people
-  # GET /api/v1/people.json
   def index
     @people = Person.all
+    render json: @people
   end
 
   # GET /api/v1/people/1
-  # GET /api/v1/people/1.json
   def show
+    render json: @person
   end
 
   # POST /api/v1/people
-  # POST /api/v1/people.json
   def create
     @person = Person.new(person_params)
 
     if @person.save
-      render :show, status: :created, location: @person
+      render json: @person, status: :created
     else
       render json: @person.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/people/1
-  # PATCH/PUT /api/v1/people/1.json
   def update
     if @person.update(person_params)
-      render :show, status: :ok, location: @person
+      render json: @person, status: :ok
     else
       render json: @person.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/people/1
-  # DELETE /api/v1/people/1.json
   def destroy
     @person.destroy
   end
