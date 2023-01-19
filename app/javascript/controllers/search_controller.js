5d84cd71618391ce05174c628313134cb5effc6c
Elson Otake <elsonotake@yahoo.com.br>
2023-01-18 21:15:39 -0300

    Add input method to search controller" git status

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,6 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  connect() {
+  static targets = [ "keyword" ]
+  const articles = document.querySelectorAll("article")
+
+  search() {
+
+  }
+
+  input() {
+    articles.forEach(article => {
+      // console.log(elem.firstChild.nextSibling.text);
+      // console.log(elem.className);
+      const isVisible = article.firstChild.nextSibling.text.toLowerCase().includes(this.keyword.toLowerCase());
+      article.classList.toggle("hide", !isVisible);
+    })
+  }
+
+  get keyword() {
+    return this.keywordTarget.value
   }
 }
