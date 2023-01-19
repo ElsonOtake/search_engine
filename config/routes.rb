50720f98f175c5d014a4f963e8ebd036b1d2131e
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 14:38:53 -0300

    Add analytics path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :analytics
+      resources :people, only: %i[index show] do
+        resources :analytics, only: %i[index show create]
+      end
     end
   end
   root 'sessions#new'
