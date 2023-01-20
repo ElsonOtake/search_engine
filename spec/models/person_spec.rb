f9c71a88eda56f302abfb1cbe8a3a306452e1af3
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 22:25:17 -0300

    Add tests to person model

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  before(:each) do
+    @person = Person.new(name: 'Search')
+  end
+
+  context 'New person' do
+    it 'is valid with valid attributes' do
+      expect(@person).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      @person.name = nil
+      expect(@person).to_not be_valid
+    end
+
+    it 'is not valid with blank name' do
+      @person.name = ' '
+      expect(@person).to_not be_valid
+    end
+  end
+end
