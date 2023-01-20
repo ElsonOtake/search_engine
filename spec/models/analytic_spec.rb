b0ebc15727f75e2ac7bd642a1fc0edf26c0130e1
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 22:26:04 -0300

    Add tests to analytic model

---
+++ spec/models/analytic_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Analytic, type: :model do
+  before(:each) do
+    @person = Person.create(name: 'Analytic Jones')
+    @analytic = Analytic.new(keyword: "Open Source", results: 12, person_id: @person.id)
+  end
+
+  context 'New analytic' do
+    it 'is valid with valid attributes' do
+      expect(@analytic).to be_valid
+    end
+
+    it 'is not valid without a keyword' do
+      @analytic.keyword = nil
+      expect(@analytic).to_not be_valid
+    end
+
+    it 'is not valid with non positive number on results' do
+      @analytic.results = -5
+      expect(@analytic).to_not be_valid
+    end
+
+    it 'is not valid without person_id' do
+      @analytic.person_id = nil
+      expect(@analytic).to_not be_valid
+    end
+  end
+end
