1d9fc7b2c5c631c4328a2ad74fb3185163ed42f3
Elson Otake <elsonotake@yahoo.com.br>
2023-01-19 21:42:47 -0300

    Update endpoints routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :people, only: %i[index show] do
-        resources :analytics, only: %i[index show create]
+      resources :people do
+        resources :analytics
       end
     end
   end
